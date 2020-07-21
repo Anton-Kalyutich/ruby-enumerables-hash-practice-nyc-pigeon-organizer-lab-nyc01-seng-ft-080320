48c11c1c10eb33e8d05afd5a7e10382074bda38a
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-21 02:57:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,20 +9,6 @@ def nyc_pigeon_organizer(data)
           the_hash[name] = {}
           if !the_hash[name].keys.include?(attribute)
         
-    end
-  end
-  
-  pigeon_names.flatten!.uniq!
-  pigeon_names.each do |name|
-    pigeon_attributes.each do |attribute|
-      the_hash[name] = {}
-      the_hash[name][attribute] = []
-    end
-  end
-  
-  data.each_value do |hash|
-    hash.each do |property, names|
-      
   
   
   the_hash
